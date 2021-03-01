816aa8a75c932c0ce264f58a930b635afbef2bd0
Zeev Pei <zeevush0819@gmail.com>
2021-03-01 12:42:25 +0000

    load_data(), tokenize() and build_model() were implemented in train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,20 +1,109 @@
+# download necessary NLTK data
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+# Import libraries
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 import sys
 
 
 def load_data(database_filepath):
-    pass
+    """Load data from sql table as a dataframe.
+    Return messages, categories as vectors and category names.
+
+    Keyword arguments:
+    database_filepath -- filepath to the database from which to take data
+    """
+    # convert sql table into a dataframe
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('message_category', engine)
+
+    X = df['message'].values
+    Y = df.iloc[:,4:]
+    category_names = list(df.columns[4:])
+
+    return X, Y, category_names
+
 
 
 def tokenize(text):
-    pass
+    """Replace url in a message with `url_placeholder` 
+    tokenize and lemmatize message
+    normalize case and delete leading/trailing whitespaces
+
+    Keyword arguments:
+    text -- a message to tokenize
+    """
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    # get list of all urls using regex
+    detected_urls = re.findall(url_regex, text)
+    
+    # replace each url in text string with placeholder
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok.strip().lower(), pos='v')
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
 
 
 def build_model():
-    pass
+    """Build a pipeline for ML model and set parameters for grid search.
+    Return the model.
+    """
+    # Build a pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(KNeighborsClassifier())),
+    ]) 
+
+    # Perform grid search
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        #'vect__max_df': (0.5, 0.75, 1.0),
+        #'vect__max_features': (None, 5000, 10000),
+        #'tfidf__use_idf': (True, False),
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return pipeline 
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    Y_pred = pd.DataFrame(Y_pred, columns=Y_test.columns, index=Y_test.index)
+
+    for col1, col2 in zip(Y_test.columns, Y_pred.columns):
+        print(col1+':')
+        print(classification_report(Y_test[col1], Y_pred[col2]))
 
 
 def save_model(model, model_filepath):
